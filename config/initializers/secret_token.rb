64ab005b74b808d1d914ae4e6d2289b87165ea46
keviv vivek <vivekpatna52@gmail.com>
2012-10-04 12:53:50 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sociorent::Application.config.secret_token = '71120948385429608d3d0616fc3247c1654798ec0a235140ae95431115f2cedf7ee6451361be6a22f649728bce7f8beb4cab889a9958f489f42767df8005fe11'

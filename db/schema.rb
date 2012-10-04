010072c46401e18973339d25cd23b6a6c24ddc9c
keviv vivek <vivekpatna52@gmail.com>
2012-10-04 18:39:40 +0530

    added books model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121004101514) do
+ActiveRecord::Schema.define(:version => 20121004130115) do
+
+  create_table "books", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "rails_admin_histories", :force => true do |t|
     t.text     "message"

76fdfd979d6e75c559fd64da0a7cb6407656733e
Senthil <psenthil202@gmail.com>
2012-10-12 09:31:47 +0530

    Updated gemfile and deploy script

--- Gemfile
+++ Gemfile
@@ -22,6 +22,10 @@ end
 
 gem 'jquery-rails'
 
+
+gem 'therubyracer'
+gem 'execjs'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,10 @@
 set :application, 'sociorent'
 set :scm, :git
 set :repository, "git@github.com:sanandnarayan/SocioRent.git"
-set :scm_passphrase, ""
 set :user, "root"
+set :scm_passphrase, ""
+set :branch, "master"
+set :deploy_via, :remote_cache
 require 'capistrano/ext/multistage'
 set :stages, ["staging", "development"]
 set :default_stage, "development"

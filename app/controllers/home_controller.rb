a8e8ce1cf35cf39d23098462ecda10efe158eaa1
keviv vivek <vivekpatna52@gmail.com>
2012-10-21 22:35:14 +0530

    bug fix : when logged in with admin fixed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,27 +9,30 @@ class HomeController < ApplicationController
       @cart = cart.books
       # Intelligent Books
       # Using FB friends
-      friend_uids = JSON.parse @user.friends
-      all_uids = JSON.parse User.select("uid").to_json()
-      friends_who_use_sociorent_uids = all_uids & friend_uids
-      # get all books their friends ordered
-      @intelligent_books = []
-      friends_who_use_sociorent_uids.each do |uid|
-        friend = User.where(:uid => uid["uid"]).first
-        # check condition on friend from the General Setting
-        general = General.first
-        case general.intelligent_book
-          when "All friends"
-            @intelligent_books += friend.books
-          when "Friends in same College"    
-            @intelligent_books += friend.books if friend.college == @user.college
-          when "Friends in same College and Stream" 
-            @intelligent_books += friend.books if friend.college == @user.college && friend.stream == @user.stream
+      if @user.friends
+        friend_uids = JSON.parse @user.friends
+        all_uids = JSON.parse User.select("uid").to_json()
+        friends_who_use_sociorent_uids = all_uids & friend_uids
+        # get all books their friends ordered
+        @intelligent_books = []
+        friends_who_use_sociorent_uids.each do |uid|
+          friend = User.where(:uid => uid["uid"]).first
+          # check condition on friend from the General Setting
+          general = General.first
+          case general.intelligent_book
+            when "All friends"
+              @intelligent_books += friend.books
+            when "Friends in same College"    
+              @intelligent_books += friend.books if friend.college == @user.college
+            when "Friends in same College and Stream" 
+              @intelligent_books += friend.books if friend.college == @user.college && friend.stream == @user.stream
+          end
         end
-      end
-      puts @intelligent_books
-      @intelligent_books.uniq
-      if @intelligent_books.count == 0
+        @intelligent_books.uniq
+        if @intelligent_books.count == 0
+          @intelligent_books = Book.offset(rand(Book.count)).first(10)
+        end
+      else
         @intelligent_books = Book.offset(rand(Book.count)).first(10)
       end
   		# render inner when user is logged in

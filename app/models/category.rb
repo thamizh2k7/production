9861b5a5248f37ac0ce2f5757a58073d58f809e0
keviv vivek <vivekpatna52@gmail.com>
2012-11-07 11:35:27 +0530

    admin change

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :books_attributes
 
   has_many :books
+
+  accepts_nested_attributes_for :books, :allow_destroy => true
 end

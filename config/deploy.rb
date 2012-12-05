c59625716d9ce8bee3cd76883ecbede903c98308
Senthil <psenthil202@gmail.com>
2012-12-06 03:31:40 +0530

    small change to deploy code , now ddefault is production

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,7 @@ set :branch, "master"
 set :deploy_via, :remote_cache
 require 'capistrano/ext/multistage'
 set :stages, ["staging", "development"]
-set :default_stage, "development"
+set :default_stage, "production"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
+require 'bundler/capistrano'

33e1b6a593dacd3300098bdb9db5307379d355ea
Senthil <psenthil202@gmail.com>
2012-12-17 16:00:21 +0530

    Cap file updated

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -16,7 +16,8 @@ after 'deploy:update_code' do
   run "rm #{release_path}/log"
   run "mkdir -p #{release_path}/log;"
   run "chmod -R 777 #{release_path}/log"
-  run "ln -s #{release_path}/public/system/images #{shared_path}"
+  run "mkdir -p 777 #{release_path}/public/system/"
+  run "ln -s #{shared_path}/images #{release_path}/public/system/" 
 end 
 
 namespace :deploy do

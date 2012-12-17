0609f97d24d2e3029472b8b2a0dec1d8a6ca21e5
Senthil <psenthil202@gmail.com>
2012-12-17 16:51:06 +0530

    Slider fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121207203007) do
+ActiveRecord::Schema.define(:version => 20121217083948) do
 
   create_table "ambassadors", :force => true do |t|
     t.integer  "college_id"

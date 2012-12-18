5c18d8b33061d623e9e4784d15922c5a4d51b724
THAMIZH <thamizh@devbrother.com>
2012-12-18 17:41:40 +0530

    google analytics,meta,fonts done

--- app/assets/javascripts/global/application.js
+++ app/assets/javascripts/global/application.js
@@ -54,7 +54,7 @@ $(document).ready(function(){
 	$(".open_university").click(function(){
 			$("#university_dialog").dialog("open");	
 	});
-	if($("#page_not_found").html()!="")
+	if($("#page_not_found").html().trim()!="")
 		$("#page_not_found").fadeIn(1000).delay(5000).fadeOut(300);
 	$(".rails_notice").delay(5000).fadeOut(300);
 });

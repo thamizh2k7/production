569b0e87ab01541e21a3805dd2502348329e9a29
Senthil <psenthil202@gmail.com>
2012-12-19 13:16:19 +0530

    Publisher Import updated

--- lib/publish_import.rb
+++ lib/publish_import.rb
@@ -1,15 +1,30 @@
 require 'csv'
 
 csvfile = File.read("tmp/x.csv")
-CSV.parse(csvfile) do |row|
-  if row[26] !="" && row[26]!="0" && row[26]!=" - "
-	publisher = Publisher.where(:name=>row[26]).first
-	book = Book.where(:isbn13 => row[7]).first
-	if publisher
-	puts "updating success" if book.update_attributes(:publisher_id => publisher.id)
-	else
-	puts "No publisher with name #{row[26]}"
-	end
-  end
-end
+# loop in book objects
+# remove images
+# get new image from csv
+# upload image
 
+CSV.parse(csvfile) do |row|
+
+	isbn = row[4]
+	book = Book.where(:isbn13 => isbn).first
+	if book
+		puts "inside #{book.id}"
+		book.images.all.each {|a| a.destroy}
+		puts "destroyed images of #{book.id}"
+		book.images.create(:image_url => row[13])
+		puts row[13]
+		puts " ---------------- "
+	end
+ #  if row[26] !="" && row[26]!="0" && row[26]!=" - "
+	# publisher = Publisher.where(:name=>row[26]).first
+	# book = Book.where(:isbn13 => row[7]).first
+	# if publisher
+	# puts "updating success" if book.update_attributes(:publisher_id => publisher.id)
+	# else
+	# puts "No publisher with name #{row[26]}"
+	# end
+ #  end
+end

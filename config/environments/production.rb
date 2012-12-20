f183bc9bcfa1992e46a226b9e6b56163df22f094
keviv vivek <vivekpatna52@gmail.com>
2012-12-20 13:47:39 +0530

    email server changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,12 +71,12 @@ Sociorent::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'www.sociorent.in' }
+  config.action_mailer.default_url_options = { :host => 'www.sociorent.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'www.sociorent.com',
+    :domain               => 'baci.lindsaar.net',
     :user_name            => 'alert@sociorent.com',
     :password             => 'Sociorent1',
     :authentication       => 'plain',

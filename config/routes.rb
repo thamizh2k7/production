1613f85dd7713c575d0ec3c5319cee90d4438731
THAMIZH <thamizh@devbrother.com>
2013-01-21 11:32:20 +0530

    Rebased for autosuggestion

--- config/routes.rb
+++ config/routes.rb
@@ -31,19 +31,13 @@ Sociorent::Application.routes.draw do
   match "pincodes" =>"home#gharpay_pincode"
   match "getSignature" =>"home#citrus_signature"
   match "/verify_code" => "orders#verify_code"
-<<<<<<< HEAD
   match "/print_label" => "home#print_label"
   match "/print_invoice" => "home#print_invoice"
-=======
   match "/get_colleges" =>"users#get_colleges"
   match "/get_streams" => "users#get_streams"
-  mount RailsAdmin::Engine => '/cb_admin', :as => 'rails_admin'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
->>>>>>> 6014c77... Autosuggestion done
-  
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   devise_scope :user do
     get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
   end

30a2574d6256f248da2f3df56fc31e2712cf53d3
Marudhu <marudhupandiyang@gmail.com>
2013-01-22 23:41:56 +0530

    Added Order Details to the Shipping page Fixes#22

--- app/admin/book_orders.rb
+++ app/admin/book_orders.rb
@@ -6,11 +6,35 @@ ActiveAdmin.register BookOrder do
 	config.clear_action_items!
 	index do
 		selectable_column
-		column :book_id do |id|
-			Book.find(id).name
+
+		column :order_id  do |book_order|
+			link_to book_order.order_id , admin_order_path(book_order)
 		end
+
+		column :user_id do |book_order|
+			link_to User.find(book_order.order.user_id).name ,admin_user_path(User.find(book_order.order.user_id))
+		end
+
+		column :book_id do |book_order|
+			link_to Book.find(book_order.book_id).name , admin_book_path(Book.find(book_order.book_id))
+		end
+
+		column :Total_Rental do |book_order|
+			"Rs. " +  book_order.order.rental_total.to_s
+		end
+
+		column :Total_Amount do |book_order|
+			"Rs. " +  book_order.order.total.to_s
+		end
+
+		column :Payment do |book_order|
+			book_order.order.payment_done ? "Done" : "Not Done"
+		end
+
+
 		column "Order Date", :created_at
-		if params[:scope] == "shipped"
+
+		if params[:scope] == "Shipped"
 			column :order_id
 			column :courier_name
 			column :tracking_number

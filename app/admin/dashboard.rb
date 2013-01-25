c8b424cc41be360a49b0953f1b2b1e56eedec573
marudhupandiyang <marudhupandiyang@gmail.com>
2013-01-26 02:35:44 +0530

    Added Statistics to Dashboard Fixes #46

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -12,12 +12,12 @@ ActiveAdmin.register_page "Dashboard" do
 
   
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+    # div :class => "blank_slate_container", :id => "dashboard_default_message" do
+    #   span :class => "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
   
     # Here is an example of a simple dashboard with columns and panels.
     #
@@ -38,5 +38,77 @@ ActiveAdmin.register_page "Dashboard" do
     #     end
     #   end
     # end
+
+
+
+    columns do        
+        
+        column do
+            panel "Last Shipped(5)" do
+                ul do
+                    Order.limit(5).order("id desc").where("status = 4").map do |order|
+                        li link_to("#" + order.id.to_s ,ab_admin_order_path(order)) + " ( Amount: " + order.total.to_s + "  By: " + link_to(order.user.name, ab_admin_user_path(order.user)) + ") "
+                    end
+                end
+            end
+        end
+
+        column do
+            panel "Last Unshipped(5)" do
+                ul do
+                    Order.limit(5).order("id desc").where("status = 2").map do |order|
+                        li link_to("#" + order.id.to_s ,ab_admin_order_path(order)) + " ( Amount: " + order.total.to_s + "  By: " + link_to(order.user.name, ab_admin_user_path(order.user)) + ") "
+                    end
+                end
+            end
+        end
+
+        column do
+            panel "Last Cancelled(5)" do
+                ul do
+                    Order.limit(5).order("id desc").where("status = 4").map do |order|
+                        li link_to("#" + order.id.to_s ,ab_admin_order_path(order)) + " ( Amount: " + order.total.to_s + "  By: " + link_to(order.user.name, ab_admin_user_path(order.user)) + ") "
+                    end
+                end
+            end
+        end
+
+    end
+
+    columns do
+
+        column do
+            panel "Recent Users(5)" do
+                ul do
+                    User.limit(5).order("id desc").map do |user|
+                        li link_to(user.name,ab_admin_user_path(user))
+                    end
+                end
+            end
+        end
+
+        column do
+            panel "Recent Orders(5)" do
+                ul do
+                    Order.limit(5).order("id desc").map do |order|
+                        li link_to("#" + order.id.to_s ,ab_admin_order_path(order)) + " ( Amount: " + order.total.to_s + "  By: " + link_to(order.user.name, ab_admin_user_path(order.user)) + ") "
+                    end
+                end
+            end
+        end
+
+        column do
+            panel "Recent Books(5)" do
+                ul do
+                    Book.limit(5).order("id desc").map do |book|
+                        li link_to(book.name ,ab_admin_book_path(book))
+                    end
+                end
+            end
+        end
+
+    end
+    
+
  end
 end

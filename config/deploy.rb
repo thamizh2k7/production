c735c513801535b8160da91452962f04bd862ef6
Senthil <psenthil202@gmail.com>
2013-02-13 23:38:20 +0530

    Deploy script updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :scm_passphrase, ""
 set :branch, "master"
 set :deploy_via, :remote_cache
 require 'capistrano/ext/multistage'
-set :stages, ["staging", "development"]
-set :default_stage, "production"
+set :stages, ["production", "development"]
+set :default_stage, "development"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true

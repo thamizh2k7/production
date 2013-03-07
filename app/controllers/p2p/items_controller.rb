517fa0138bbc934242801aba63b5051e388b9c2e
marudhupandiyang <marudhupandiyang@gmail.com>
2013-03-07 20:17:03 +0530

    Added Csv auto generate for template

--- app/controllers/p2p/items_controller.rb
+++ app/controllers/p2p/items_controller.rb
@@ -625,4 +625,33 @@ class P2p::ItemsController < ApplicationController
       redirect_to "/p2p/paymentdetails/bought"
     end
   end
+
+
+  def downloadtemplate
+
+    if params.has_key?(:category_template)
+
+        cat = P2p::Category.find(params[:category_template])
+
+        require 'csv'
+        
+        
+        item = ['brand','title','price','condition','location','desc','image1','image2','image3']
+
+        item = item + cat.specs.pluck('name')
+
+        csv_string = CSV.generate do |csv|
+          csv << item
+        end
+
+        send_data(csv_string ,:filename => cat.name + ".csv")
+        return
+
+    else
+      @cat = P2p::Category.all
+    end
+
+  end
+
+
 end

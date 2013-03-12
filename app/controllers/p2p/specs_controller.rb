4226e27e8926caabc9b3be52b41599ff263057cd
marudhupandiyang <marudhupandiyang@gmail.com>
2013-03-12 12:47:52 +0530

    code clean

--- app/controllers/p2p/specs_controller.rb
+++ app/controllers/p2p/specs_controller.rb
@@ -1,11 +1,9 @@
 class P2p::SpecsController < ApplicationController
-
   layout :p2p_layout
   # GET /p2p/specs
   # GET /p2p/specs.json
   def index
     @p2p_specs = P2p::Spec.all
-
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @p2p_specs }
@@ -16,7 +14,6 @@ class P2p::SpecsController < ApplicationController
   # GET /p2p/specs/1.json
   def show
     @p2p_spec = P2p::Spec.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @p2p_spec }
@@ -27,8 +24,6 @@ class P2p::SpecsController < ApplicationController
   # GET /p2p/specs/new.json
   def new
     @p2p_spec = P2p::Spec.new
-
-
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @p2p_spec }
@@ -44,14 +39,10 @@ class P2p::SpecsController < ApplicationController
   # POST /p2p/specs.json
   def create
     @p2p_spec = P2p::Spec.new(params[:p2p_spec])
-
-
     respond_to do |format|
       if @p2p_spec.save
-
-         redirect_to p2p_specs_url, notice: 'Specification was successfully created.'
-         return
-
+        redirect_to p2p_specs_url, notice: 'Specification was successfully created.'
+        return
         format.html { redirect_to @p2p_spec, notice: 'Spec was successfully created.' }
         format.json { render json: @p2p_spec, status: :created, location: @p2p_spec }
       else
@@ -65,16 +56,10 @@ class P2p::SpecsController < ApplicationController
   # PUT /p2p/specs/1.json
   def update
     @p2p_spec = P2p::Spec.find(params[:id])
-
-
     respond_to do |format|
       if @p2p_spec.update_attributes(params[:p2p_spec])
-
         redirect_to p2p_specs_url, notice: 'Specification was successfully created.'
-
-        return 
-
-        
+        return
         format.html { redirect_to @p2p_spec, notice: 'Spec was successfully updated.' }
         format.json { head :no_content }
       else
@@ -89,10 +74,10 @@ class P2p::SpecsController < ApplicationController
   def destroy
     @p2p_spec = P2p::Spec.find(params[:id])
     @p2p_spec.destroy
-
     respond_to do |format|
       format.html { redirect_to p2p_specs_url }
       format.json { head :no_content }
     end
   end
+  
 end

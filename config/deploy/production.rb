6a60301be86d110d630df289df45f2d4853b75a5
Senthil <psenthil202@gmail.com>
2013-03-20 00:18:25 +0530

    Deploy scrip updated

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,29 +1,36 @@
+require "rvm/capistrano"
+require "bundler/capistrano"
+
 server "103.8.126.71", :app, :web, :db, :primary => true
 set :deploy_to, "/var/www/sociorent.com"
 set :branch, 'master'
 set :scm_verbose, true
 set :use_sudo, false
 set :rails_env, "production" #added for delayed job 
+set :rvm_type, :system
 
 
 after 'deploy:update_code' do
-
-  # run "cd #{release_path}; bundle install; RAILS_ENV=production rake assets:precompile;"
-  run "cd #{release_path}; RAILS_ENV=production"
+  # run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
+  run "cd #{release_path};"
   run "mkdir -p #{release_path}/tmp/cache;"
   run "chmod -R 777 #{release_path}/tmp/cache;"
   run "mkdir -p #{release_path}/public/uploads;"
   run "chmod -R 777 #{release_path}/public/uploads"
-  run "rm #{release_path}/log"
-  run "mkdir -p #{release_path}/log;"
-  run "chmod -R 777 #{release_path}/log"
   run "rm -rf #{release_path}/public/system"
-  # run "mkdir -p 777 #{release_path}/public/system/"
+  run "unlink #{release_path}/public/db_admin"
+  run "unlink #{release_path}/public/blog"
+
   run "ln -s #{shared_path}/system/ #{release_path}/public/" 
   run "ln -s '/var/www/blog' #{release_path}/public/" 
   run "ln -s '/var/www/db_admin' #{release_path}/public/"
-  # ln -s /var/www/sociorent.com/shared/system/ /var/www/sociorent.com/current/public/
-end 
+
+  run "cd #{release_path} && bundle --deployment"
+  run "cd #{release_path} && RAILS_ENV=production rake db:migrate"
+  run "cd #{release_path} && RAILS_ENV=production rake assets:precompile"
+  run "chown -R www-data:www-data #{release_path}/*"
+  run "chmod -R 777 #{release_path}/log"
+end
 
 namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do

a2a4b2189cd76eee39334e4bb72540dc56194d04
thamizh <thamizh@devbrother.com>
2013-07-02 12:01:29 +0530

    updated deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 set :application, 'sociorent'
 set :scm, :git
-set :repository, "git@github.com:sociorent/production.git"
+set :repository, "git@github.com:thamizh/production.git"
 # set :repository, "https://github.com/sociorent/production.git"
 set :user, "root"
 set :scm_passphrase, ""
-set :branch, "master"
+set :branch, "p2p_dep"
 set :deploy_via, :remote_cache
 require 'capistrano/ext/multistage'
 set :stages, ["production", "development"]
